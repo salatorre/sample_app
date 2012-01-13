2876c8082c733abfe4a5cf87ee26b04a3bc67ae0
Steven Alatorre <steve.alatorre@gmail.com>
2012-01-13 15:56:20 -0800

    Adding gravatar

--- app/models/user.rb
+++ app/models/user.rb
@@ -16,18 +16,35 @@ class User < ActiveRecord::Base
   def has_password?(submitted_password)
     #compare encrypted_password with the encrypted version of
     #submitted password
+    encrypted_password == encrypt(submitted_password)
+  end
+
+  def self.authenticate(email, submitted_password)
+    user = find_by_email(email)
+    return nil if user.nil?
+    return user if user.has_password?(submitted_password)
   end
 
   private
 
     def encrypt_password
+      self.salt = make_salt if new_record?
       self.encrypted_password = encrypt(self.password)
     end
 
     def encrypt(string)
-      string #only temporary
+      secure_hash("#{salt}--#{string}")
+    end
+
+    def make_salt
+      secure_hash("#{Time.now.utc}--#{password}")
+    end
+
+    def secure_hash(string)
+      Digest::SHA2.hexdigest(string)
     end
 end
+
 
 
 # == Schema Information
@@ -40,5 +57,6 @@ end
 #  created_at         :datetime
 #  updated_at         :datetime
 #  encrypted_password :string(255)
+#  salt               :string(255)
 #
 
